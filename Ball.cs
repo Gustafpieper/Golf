71fb7f4cb5b86e088a6f0c42cb96d53f67c16be5
Gustafpieper <113342781+Gustafpieper@users.noreply.github.com>
2025-02-04 13:15:37 +0100

    skjuta rätt och åt rätt håll

--- Ball.cs
+++ Ball.cs
@@ -12,23 +12,27 @@ namespace EasyStart
     internal class Ball : Actor
     {
         private float time = 0;
-        private float speed = 10;
+        private float speed = 0;
+        private bool hasPressedSpace = false;
         public override void Act()
         {
             time = time + 1;
 
             var mouseState = Mouse.GetState();
             TurnTowards(mouseState.X, mouseState.Y);
+            Turn(180);
 
-            if (time > 100)
-            {
-                speed -= 0.07f;
-                if (speed < 0)
-                {
-                    speed = 0;
-                }
-                Move(speed);
+            if (Keyboard.GetState().IsKeyDown(Keys.Space) && !hasPressedSpace) {
+                speed = 10f;
+                
             }
+            speed -= 0.07f;
+            if (speed < 0)
+            {
+                speed = 0;
+                hasPressedSpace = false;
+            }
+            Move(speed);
 
 
 
